c608a0f946225bec3290459034cb45e4fc53e4b3
RuslanAndrianov <ruslandomoney@mail.ru>
2024-05-23 20:51:01 +0300

    feat: changed table games

--- migrations/create_table_games.sql
+++ migrations/create_table_games.sql
@@ -1,9 +1,13 @@
 --liquibase formatted sql
 --changeset RuslanAndrianov:created table links
 CREATE TABLE games(
-    id        BIGINT,
-    name      VARCHAR(50)    NOT NULL,
-    price     INTEGER        NOT NULL,
-    people    VARCHAR(15)    NOT NULL,
+    id             BIGINT,
+    name           VARCHAR(50)      NOT NULL,
+    price          INTEGER          NOT NULL,
+    people         VARCHAR(15)      NOT NULL,
+    picture        VARCHAR(8192)    NOT NULL,
+    type           VARCHAR(30)      NOT NULL,
+    description    VARCHAR(200)     NOT NULL,
+
     PRIMARY KEY(id)
 );
