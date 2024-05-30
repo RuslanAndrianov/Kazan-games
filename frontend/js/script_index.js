8c8bb2b59842875333b44f9d0fa355e489f78552
RuslanAndrianov <ruslandomoney@mail.ru>
2024-05-30 10:44:32 +0300

    refactor: fixed typo

--- frontend/js/script_index.js
+++ frontend/js/script_index.js
@@ -1,55 +1,5 @@
 import { GamesTab } from "../js/react-components/games-tab.js"
 
-// Создание табов "Мировые хиты", "Игры для компании" и "Семейные играми"
-
-ReactDOM.render(React.createElement(
-    "div",
-    null,
-    React.createElement(GamesTab, null),
-    React.createElement(GamesTab, null),
-    React.createElement(GamesTab, null)
-), document.getElementsByClassName("games")[0]);
-
-// Переключение между "Мировыми хитами", "Играми для компании" и "Семейными играми"
-
-const mainGamesChoiseWrap = document.getElementsByClassName("main-games-choises")[0];
-const mainGamesChoises = document.getElementsByClassName("main-games-choise");
-const mainGamesTabContent = document.getElementsByClassName("main-games-tab");
-
-function hideTabContent(a) {
-    for (let i = a; i < mainGamesTabContent.length; i++) {
-        mainGamesTabContent[i].classList.remove("show");
-        mainGamesTabContent[i].classList.add("hide");
-    }
-}
-
-function showTabContent(b) {
-    if (mainGamesTabContent[b].classList.contains("hide")) {
-        mainGamesTabContent[b].classList.remove("hide");
-        mainGamesTabContent[b].classList.add("show");
-    }
-}
-
-mainGamesChoiseWrap.addEventListener("click", (event) => {
-    let target = event.target;
-    if (target && target.classList.contains("main-games-choise")) {
-        for (let i = 0; i < mainGamesChoises.length; i++) {
-            mainGamesChoises[i].classList.remove("choise");
-        }
-        
-        for (let i = 0; i < mainGamesChoises.length; i++) {
-            if (target === mainGamesChoises[i]) {
-                target.classList.add("choise");
-                hideTabContent(0);
-                showTabContent(i);
-                break;
-            }
-        }
-    }
-});
-
-hideTabContent(1);
-
 // Получение данных с БД
 
 const request = new XMLHttpRequest();
@@ -75,3 +25,53 @@ request.onreadystatechange = (e) => {
         }
     }
 }
+
+// Создание табов "Мировые хиты", "Игры для компании" и "Семейные играми"
+
+ReactDOM.render(React.createElement(
+    "div",
+    null,
+    React.createElement(GamesTab, null),
+    React.createElement(GamesTab, null),
+    React.createElement(GamesTab, null)
+), document.getElementsByClassName("games")[0]);
+
+// Переключение между "Мировыми хитами", "Играми для компании" и "Семейными играми"
+
+const mainGamesChoiseWrap = document.getElementsByClassName("main-games-choices")[0];
+const mainGamesChoises = document.getElementsByClassName("main-games-choice");
+const mainGamesTabContent = document.getElementsByClassName("main-games-tab");
+
+function hideTabContent(a) {
+    for (let i = a; i < mainGamesTabContent.length; i++) {
+        mainGamesTabContent[i].classList.remove("show");
+        mainGamesTabContent[i].classList.add("hide");
+    }
+}
+
+function showTabContent(b) {
+    if (mainGamesTabContent[b].classList.contains("hide")) {
+        mainGamesTabContent[b].classList.remove("hide");
+        mainGamesTabContent[b].classList.add("show");
+    }
+}
+
+mainGamesChoiseWrap.addEventListener("click", (event) => {
+    let target = event.target;
+    if (target && target.classList.contains("main-games-choice")) {
+        for (let i = 0; i < mainGamesChoises.length; i++) {
+            mainGamesChoises[i].classList.remove("choice");
+        }
+        
+        for (let i = 0; i < mainGamesChoises.length; i++) {
+            if (target === mainGamesChoises[i]) {
+                target.classList.add("choice");
+                hideTabContent(0);
+                showTabContent(i);
+                break;
+            }
+        }
+    }
+});
+
+hideTabContent(1);
